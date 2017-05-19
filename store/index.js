79bd85b4a62b4af694cd6d12dc390ab840b9da0f
NicoPennec <nemohp@yahoo.fr>
2017-05-20 00:30:18 +0200

    declare Store as module

--- store/index.js
+++ store/index.js
@@ -1,20 +1,15 @@
-import Vuex from 'vuex'
+export const state = {
+  ssr: null,
+  city: null
+}
 
-const store = new Vuex.Store({
-  state: {
-    ssr: null,
-    city: null
+export const mutations = {
+  SET_RENDERER (state, value) {
+    console.log('SET_RENDERER', value)
+    state.ssr = value
   },
-  mutations: {
-    SET_RENDERER (state, value) {
-      console.log('SET_RENDERER', value)
-      state.ssr = value
-    },
-    SET_CITY (state, value) {
-      console.log('SET_CITY', value)
-      state.city = value
-    }
+  SET_CITY (state, value) {
+    console.log('SET_CITY', value)
+    state.city = value
   }
-})
-
-export default store
+}
