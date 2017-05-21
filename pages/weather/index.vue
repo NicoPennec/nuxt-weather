060a3c4b3c7ca55171fc7e8756a3d9971ff509c1
NicoPennec <nemohp@yahoo.fr>
2017-05-21 23:33:48 +0200

    auto select city

--- pages/weather/index.vue
+++ pages/weather/index.vue
@@ -1,2 +1,14 @@
 <template>
 </template>
+
+<script>
+  export default {
+    asyncData ({ redirect, store }) {
+      // if city redirect
+      if (store.state.city) {
+        redirect(`weather/${store.state.city}`)
+      }
+      return {}
+    }
+  }
+</script>
