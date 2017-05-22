c96ca8d9ba7aa655ef32b283c8f9a7485190557b
NicoPennec <nemohp@yahoo.fr>
2017-05-22 15:28:38 +0200

    fix external link

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <section>
     <h2>About</h2>
     <p>This web site is only a POC for <abbr title="Server-Side Rendering">SSR</abbr> with Nuxtjs.</p>
-    <p>Made with love by <a href="http://pennec.io">NicoPennec</a> 🤓</p>
+    <p>Made with love by <a href="http://pennec.io" target="_blank" rel="noopener">NicoPennec</a> 🤓</p>
   </section>
 </template>
 
