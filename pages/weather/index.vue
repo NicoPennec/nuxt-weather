743c85a2f52f4b61b911db65e79e6537b32ba1c9
NicoPennec <nemohp@yahoo.fr>
2017-07-31 23:15:43 +0200

    Fix redirection

--- pages/weather/index.vue
+++ pages/weather/index.vue
@@ -1,12 +1,9 @@
-<template>
-</template>
-
 <script>
   export default {
     asyncData ({ redirect, store }) {
       // if city redirect
       if (store.state.city) {
-        redirect(`weather/${store.state.city}`)
+        redirect(`/weather/${store.state.city}`)
       }
       return {}
     }
