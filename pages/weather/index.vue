5db3487f5c5a98733930d1b6c419e3b8ef5f4793
NicoPennec <nemohp@yahoo.fr>
2017-08-26 10:15:40 +0200

    add mising template to fix SSR error

--- pages/weather/index.vue
+++ pages/weather/index.vue
@@ -1,11 +1,13 @@
+<template>
+</template>
+
 <script>
   export default {
-    asyncData ({ redirect, store }) {
-      // if city redirect
+    fetch ({ redirect, store }) {
+      // if city set, redirect to its weather page
       if (store.state.city) {
         redirect(`/weather/${store.state.city}`)
       }
-      return {}
     }
   }
 </script>
